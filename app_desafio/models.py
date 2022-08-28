d059a29add2b1d25ddc4c11ab1861aca12d5f602
Gonzalo Villanueva <tulivinci@gmail.com>
2022-08-28 19:42:35 +0200

    Desafio MVT-JulianVinci

---
+++ app_desafio/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Familiar(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    peso_kg = models.IntegerField()
+    parentesco = models.CharField(max_length=50)
+
+
+
